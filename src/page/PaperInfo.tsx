69cec41cc55f063085ae89b9aa0ce86e004334bf
fluorjo <fluorjo@naver.com>
2024-09-24 12:38:07 +0900

    다음 뉴스 통해서 일단 성공?

--- src/page/PaperInfo.tsx
+++ src/page/PaperInfo.tsx
@@ -7,8 +7,7 @@ export default function PaperInfoPage() {
   const fetchData = async () => {
     try {
       const searchQuery =
-        "https://news.nate.com/search?q=%5B%EC%98%A4%EB%8A%98%EC%9D%98+%EC%A3%BC%EC%9A%94%EC%9D%BC%EC%A0%95%5D%EC%82%AC%ED%9A%8C";
-
+        "https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%5B%EC%98%A4%EB%8A%98%EC%9D%98+%EC%A3%BC%EC%9A%94%EC%9D%BC%EC%A0%95%5D%EC%82%AC%ED%9A%8C";
       const HTMLresponse = await fetch(searchQuery);
       const htmlString = await HTMLresponse.text();
       console.log(htmlString);
