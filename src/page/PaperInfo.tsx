4ba8d3973ef4e997a7ed149f633a5a49b3da3024
fluorjo <fluorjo@naver.com>
2024-09-25 17:57:26 +0900

    링크 클릭하면 이동하는 touchable opacity

--- src/page/PaperInfo.tsx
+++ src/page/PaperInfo.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
+import * as Linking from 'expo-linking';
 
 export default function PaperInfoPage() {
   const [testhtml, settesthtml] = useState<string | null>(null);
 
   const fetchData = async () => {
     try {
-      const date = "9월22일";
-      const searchQuery = `[오늘의 주요일정]사회(${date}`;
+      const date = "2024-09-25";
+      const formattedDate = `${date.split("-")[1][1]}월${date.split("-")[2]}일`;
+      console.log(formattedDate);
+      const searchQuery = `[오늘의 주요일정]사회(${formattedDate}`;
       const encodedQuery = encodeURIComponent(searchQuery);
       const searchUrl = `https://m.search.daum.net/search?nil_profile=btn&w=news&DA=SBC&q=${encodedQuery}`;
       const HTMLresponse = await fetch(searchUrl);
@@ -15,17 +18,24 @@ export default function PaperInfoPage() {
       const htmlString = (await HTMLresponse.text())
         .split(daumUrl)[1]
         .split(`"`)[0];
-      settesthtml(`${daumUrl}${htmlString}`);
+      return `${daumUrl}${htmlString}`;
     } catch (error) {
       console.error("요청 중 오류 발생:", error);
     }
   };
+  const link = async () => {
+    const url = await fetchData()
+    if(url)
+    Linking.openURL(url)
+}
   useEffect(() => {
     fetchData();
   }, []);
   return (
     <ScrollView>
-      <Text>{testhtml}</Text>
+      <TouchableOpacity onPress={link}>
+        <Text>링크</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 }
