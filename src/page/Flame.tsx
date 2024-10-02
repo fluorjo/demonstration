ca11b5766ad51bf41ad5cd9756675ee33b221127
fluorjo <fluorjo@naver.com>
2024-10-02 14:59:49 +0900

    그림자 설정.

--- src/page/Flame.tsx
+++ src/page/Flame.tsx
@@ -1,25 +1,37 @@
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { StyleSheet, View } from "react-native";
-
+import { StyleSheet,View } from "react-native";
+import { Shadow } from "react-native-shadow-2";
 export default function Flame() {
   return (
-    <>
-      <View style={styles.thread}></View>
-      <View style={styles.glow}></View>
-      <LinearGradient
-        style={styles.flame}
-        colors={["#ffffff", "#ffffff3d"]}
-      ></LinearGradient>
-    </>
+    <View style={styles.flameContainer}>
+      {/* <View style={styles.thread}></View> */}
+      {/* <View style={styles.glow}></View> */}
+      <Shadow
+        offset={[0, -7]}
+        startColor="#f75f00a4"
+        endColor="#f780004b"
+        style={extra_styles.flameShadow}
+        distance={3}
+      >
+        <LinearGradient
+          style={styles.flame}
+          colors={["#ffffff", "#ffffff3d"]}
+        />
+      </Shadow>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  flameContainer: {
+    position: "absolute",
+    left: 40,
+    top: 40,
+  },
   flame: {
-    left: "50%",
-    top: "50%",
-    position: "relative",
+    position: "absolute",
+
     width: 24,
     height: 120,
     zIndex: 3,
@@ -30,4 +42,10 @@ const styles = StyleSheet.create({
   },
   thread: {},
   glow: {},
+});
+
+const extra_styles = StyleSheet.create({
+    flameShadow: {
+    // ...styles.flame,
+  },
 });
