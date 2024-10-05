c3d5cf34cb6d550a801b7aa844bd51847521f90f
fluorjo <fluorjo@naver.com>
2024-10-05 11:27:14 +0900

    skia로 모양 잡고 그림자 넣어보기

--- src/page/Skia.tsx
+++ src/page/Skia.tsx
@@ -1,24 +1,18 @@
-import { Canvas, Circle, Group, Skia } from "@shopify/react-native-skia";
-
-import { useState } from "react";
+import { Canvas, Path, Shadow, Skia } from "@shopify/react-native-skia";
 
 export default function SkiaSVG() {
-
-
   const r = 128;
   const svg = Skia.SVG.MakeFromString(
-    `<svg viewBox='0 0 290 500' xmlns='http://www.w3.org/2000/svg'>
+    `<svg viewBox='0 0 300 500' xmlns='http://www.w3.org/2000/svg'>
       <circle cx='31' cy='325' r='120px' fill='#c02aaa'/>
     </svg>`
   )!;
   return (
     <Canvas style={{ flex: 1 }}>
-      <Circle cx={r} cy={r} r={r} color="#51AFED" />
-      {/* The paint is inherited by the following sibling and descendants. */}
-      <Group color="lightblue" style="stroke" strokeWidth={10}>
-        <Circle cx={r} cy={r} r={r / 2} />
-        <Circle cx={r} cy={r} r={r / 3} color="white" />
-      </Group>
+      <Path path="M 50 0 C 100 180 90 150 50 300" color="lightblue" />
+      <Shadow dx={12} dy={12} blur={25} color="#ff5900" />
+      <Path path="M 50 0 C 0 180 10 150 50 300" color="red" />
+      <Shadow dx={-12} dy={-12} blur={25} color="#9500ff" />
     </Canvas>
   );
 }
