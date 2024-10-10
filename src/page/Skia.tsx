92da22cc59ab82870274ced8d733b9a40e1bf2bd
fluorjo <fluorjo@naver.com>
2024-10-10 17:36:16 +0900

    변수명 정리

--- src/page/Skia.tsx
+++ src/page/Skia.tsx
@@ -15,24 +15,28 @@ import { Animated, Easing, Text, View } from "react-native";
 export default function SkiaSVG() {
   const [startPointX, setStartPointX] = useState(50.0); // 50
   const [startPointY, setStartPointY] = useState(0.0); // 0.0
-  const [Q1ControlX, setQ1ControlX] = useState(75.0); // 75
-  const [Q1ControlY, setQ1ControlY] = useState(20.0); // 20
-  const [Q2ControlX, setQ2ControlX] = useState(25); // 25
-  const [Q2ControlY, setQ2ControlY] = useState(20); // 20
   const [Q1EndX, setQ1EndX] = useState(80.0); // 80
   const [Q1EndY, setQ1EndY] = useState(240.0); // 240
-  const [Q2End, setQ2End] = useState(350); // 350
-  const [H1, setH1] = useState(20); // 20
-  const [V2, setV2] = useState(240); // 240
-  const [Q2Control, setQ2Control] = useState((Q1EndY + Q2End) / 2);
+  const [Q1ControlX, setQ1ControlX] = useState(75.0); // 75
+  const [Q1ControlY, setQ1ControlY] = useState(20.0); // 20
+  const [Q2EndX, setQ2EndX] = useState(Q1EndX-3); 
+  const [Q2EndY, setQ2EndY] = useState(350); // 350
+  const [Q3StartX, setQ3StartX] = useState(Q2EndX-60); 
+  const [Q3StartY, setQ3StartY] = useState(Q2EndY); 
+  const [Q3EndX, setQ3EndX] = useState(Q1EndX - 63); 
+  const [Q3EndY, setQ3EndY] = useState(Q1EndY); 
+  const [Q2ControlX, setQ2ControlX] = useState(Q1EndX); // 80
+  const [Q2ControlY, setQ2ControlY] = useState((Q1EndY + Q2EndY) / 2);
   const [Q3ControlX, setQ3ControlX] = useState(15); // 25
-  const [Q3ControlY, setQ3ControlY] = useState(20); // 20
+  const [Q3ControlY, setQ3ControlY] = useState(295); // 20
+  const [Q4ControlX, setQ4ControlX] = useState(25); // 25
+  const [Q4ControlY, setQ4ControlY] = useState(20); // 20
   const animatedStartPointX = new Animated.Value(50);
   const animatedStartPointY = new Animated.Value(0);
   const animatedQ1ControlX = new Animated.Value(75);
   const animatedQ1ControlY = new Animated.Value(20);
-  const animatedQ2ControlX = new Animated.Value(25);
-  const animatedQ2ControlY = new Animated.Value(20);
+  const animatedQ4ControlX = new Animated.Value(25);
+  const animatedQ4ControlY = new Animated.Value(20);
   const animatedQ1EndX = new Animated.Value(80);
   const animatedQ1EndY = new Animated.Value(140);
 
@@ -187,25 +191,25 @@ export default function SkiaSVG() {
         ]),
         // Q2
         Animated.sequence([
-          Animated.timing(animatedQ2ControlX, {
+          Animated.timing(animatedQ4ControlX, {
             toValue: 15,
             duration: 2000,
             easing: Easing.inOut(Easing.ease),
             useNativeDriver: false,
           }),
-          Animated.timing(animatedQ2ControlX, {
+          Animated.timing(animatedQ4ControlX, {
             toValue: 35,
             duration: 2000,
             easing: Easing.inOut(Easing.ease),
             useNativeDriver: false,
           }),
-          Animated.timing(animatedQ2ControlX, {
+          Animated.timing(animatedQ4ControlX, {
             toValue: 15,
             duration: 2000,
             easing: Easing.inOut(Easing.ease),
             useNativeDriver: false,
           }),
-          Animated.timing(animatedQ2ControlX, {
+          Animated.timing(animatedQ4ControlX, {
             toValue: 25,
             duration: 2000,
             easing: Easing.inOut(Easing.ease),
@@ -213,25 +217,25 @@ export default function SkiaSVG() {
           }),
         ]),
         Animated.sequence([
-          Animated.timing(animatedQ2ControlY, {
+          Animated.timing(animatedQ4ControlY, {
             toValue: 30,
             duration: 2000,
             easing: Easing.inOut(Easing.ease),
             useNativeDriver: false,
           }),
-          Animated.timing(animatedQ2ControlY, {
+          Animated.timing(animatedQ4ControlY, {
             toValue: 10,
             duration: 2000,
             easing: Easing.inOut(Easing.ease),
             useNativeDriver: false,
           }),
-          Animated.timing(animatedQ2ControlY, {
+          Animated.timing(animatedQ4ControlY, {
             toValue: 30,
             duration: 2000,
             easing: Easing.inOut(Easing.ease),
             useNativeDriver: false,
           }),
-          Animated.timing(animatedQ2ControlY, {
+          Animated.timing(animatedQ4ControlY, {
             toValue: 20,
             duration: 2000,
             easing: Easing.inOut(Easing.ease),
@@ -292,23 +296,23 @@ export default function SkiaSVG() {
     //   // const newQ1ControlY = xx + value * random?;
     //   // setQ1ControlY(newQ1ControlY);
     // });
-    // animatedQ2ControlX.addListener(({ value }) => {
-    //   const newQ2ControlX = value;
-    //   setQ2ControlX(newQ2ControlX);
-    //   // const newQ2ControlY = xx + value * random?;
-    //   // setQ2ControlY(newQ2ControlY);
+    // animatedQ4ControlX.addListener(({ value }) => {
+    //   const newQ4ControlX = value;
+    //   setQ4ControlX(newQ4ControlX);
+    //   // const newQ4ControlY = xx + value * random?;
+    //   // setQ4ControlY(newQ4ControlY);
     // });
-    // animatedQ2ControlY.addListener(({ value }) => {
-    //   const newQ2ControlY = value;
-    //   setQ2ControlY(newQ2ControlY);
-    //   // const newQ2ControlY = xx + value * random?;
-    //   // setQ2ControlY(newQ2ControlY);
+    // animatedQ4ControlY.addListener(({ value }) => {
+    //   const newQ4ControlY = value;
+    //   setQ4ControlY(newQ4ControlY);
+    //   // const newQ4ControlY = xx + value * random?;
+    //   // setQ4ControlY(newQ4ControlY);
     // });
     // animatedQ1EndX.addListener(({ value }) => {
     //   const newQ1EndX = value;
     //   setQ1EndX(newQ1EndX);
-    //   // const newQ2ControlY = xx + value * random?;
-    //   // setQ2ControlY(newQ2ControlY);
+    //   // const newQ4ControlY = xx + value * random?;
+    //   // setQ4ControlY(newQ4ControlY);
     // });
 
     return () => {
@@ -316,29 +320,29 @@ export default function SkiaSVG() {
       animatedStartPointY.removeAllListeners();
       animatedQ1ControlX.removeAllListeners();
       animatedQ1ControlY.removeAllListeners();
-      animatedQ2ControlX.removeAllListeners();
-      animatedQ2ControlY.removeAllListeners();
+      animatedQ4ControlX.removeAllListeners();
+      animatedQ4ControlY.removeAllListeners();
     };
   }, []);
 
   const pathString = `M ${startPointX} ${startPointY} 
   Q ${Q1ControlX} ${Q1ControlY} ${Q1EndX} ${Q1EndY}  
-  Q ${Q1EndX} ${Q2Control} ${Q1EndX - 3} ${Q2End}
-  L ${Q1EndX - 60} ${Q2End}
-  Q ${Q3ControlX} ${Q2Control} ${Q1EndX - 63} ${Q1EndY}
-  Q ${Q2ControlX} ${Q2ControlY} ${startPointX} ${startPointY}
+  Q ${Q2ControlX} ${Q2ControlY} ${Q2EndX} ${Q2EndY}
+  L ${Q3StartX} ${Q3StartY}
+  Q ${Q3ControlX} ${Q3ControlY} ${Q3EndX} ${Q3EndY}
+  Q ${Q4ControlX} ${Q4ControlY} ${startPointX} ${startPointY}
   Z`;
 
   const points = [
     { x: startPointX, y: startPointY, label: "M" },
     { x: Q1ControlX, y: Q1ControlY, label: "     Q1 Control" },
     { x: Q1EndX, y: Q1EndY, label: "    Q1 end" },
-    { x: Q1EndX, y: Q2Control, label: "    Q2Control" },
-    { x: Q1EndX, y: Q2End, label: "    Q2End" },
-    { x: H1, y: Q2End, label: "    Q3 Start" },
-    { x: Q3ControlX, y: Q2Control, label: "    Q3 Control" },
-    { x: H1, y: V2, label: "Q4 start " },
-    { x: Q2ControlX, y: Q2ControlY, label: "Q4 control" },
+    { x: Q1EndX, y: Q2ControlY, label: "    Q2Control" },
+    { x: Q1EndX, y: Q2EndY, label: "    Q2End" },
+    { x: Q3StartX, y: Q3StartY, label: "    Q3 Start" },
+    { x: Q3ControlX, y: Q3ControlY, label: "    Q3 Control" },
+    { x: Q3EndX, y: Q3EndY, label: "Q4 start " },
+    { x: Q4ControlX, y: Q4ControlY, label: "Q4 control" },
     { x: startPointX, y: startPointY, label: "Z" },
   ];
 
