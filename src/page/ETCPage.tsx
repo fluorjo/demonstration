161029a0940d3d806efdb474d4caa231cef48fcb
fluorjo <fluorjo@naver.com>
2024-10-20 18:14:00 +0900

    문의메일 보내기

--- src/page/ETCPage.tsx
+++ src/page/ETCPage.tsx
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import AppStoreReivew from "./AppStoreReview";
+import { sendEmail } from "./Email";
 
 export default function ETCPage() {
   return (
     <View>
       <TouchableOpacity onPress={AppStoreReivew}>
         <Text>앱스토어 링크</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={sendEmail}>
+        <Text>메일</Text>
       </TouchableOpacity>
     </View>
   );
