2aac1d0ab0674c45f56a31317b53fd55b425996f
fluorjo <fluorjo@naver.com>
2024-10-25 20:07:35 +0900

    버튼박스 정중앙 위치.

--- src/components/FABtnContainer.tsx
+++ src/components/FABtnContainer.tsx
@@ -12,43 +12,46 @@ import { FABtn4Demo } from "./FABtn4DemoInfo";
 
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 const { width } = Dimensions.get("window");
+const animatedContainerWidth=300;
 
 export default function FloatingActionBtnContainer({ buttons }) {
   const isExpanded = useSharedValue(false);
   const rotation = useSharedValue(0);
+  const [show, setShow] = useState(false);
+
   const handlePress = () => {
     isExpanded.value = !isExpanded.value;
     rotation.value = isExpanded.value ? 0 : 45;
     setShow(!isExpanded.value);
-    // console.log("show", show);
-    console.log("isExpanded", isExpanded.value);
   };
+
   const animatedStyle = useAnimatedStyle(() => {
     return {
       transform: [
-        { translateX: width * 0.5 - 20 },
+        { translateX: 0 },
         { rotateZ: withTiming(`${rotation.value}deg`) },
       ],
     };
   });
-
-  const [show, setShow] = useState(false);
-
   return (
     <View style={styles.buttonContainer}>
-      <AnimatedPressable
-        onPress={handlePress}
-        style={[styles.shadow, mainButtonStyles.button, animatedStyle]}
-      >
-        <FontAwesome name="plus" size={24} color="black" />
-      </AnimatedPressable>
+      <Animated.View style={styles.wrapper}>
+        <AnimatedPressable
+          onPress={handlePress}
+          style={[styles.shadow, mainButtonStyles.button, animatedStyle]}
+        >
+          <FontAwesome name="plus" size={24} color="black" />
+        </AnimatedPressable>
+      </Animated.View>
+
       {show && (
         <Animated.View
-          style={styles.animatedContainer}
+          style={[styles.animatedContainer]}
           entering={StretchInY}
           exiting={StretchOutY}
-        />
+        ></Animated.View>
       )}
+
       {buttons.map((button, index) => (
         <FABtn4Demo
           isExpanded={isExpanded}
@@ -69,14 +72,10 @@ const mainButtonStyles = StyleSheet.create({
     height: 40,
     width: 40,
     borderRadius: 100,
-    backgroundColor: "#7f7f7f9b",
+    backgroundColor: "#ffffff9b",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    transform: [
-      { translateX: width * 0.5 - 20 },
-      // ,{rotateZ:'45deg'}
-    ],
   },
 });
 
@@ -90,7 +89,10 @@ const styles = StyleSheet.create({
     height: 50,
     bottom: 20,
     zIndex: 1,
-    // backgroundColor: "blue",
+  },
+  wrapper: {
+    position: "relative",
+    transform: [{ translateX: width * 0.5 - 20 }],
   },
   shadow: {
     shadowColor: "#171717",
@@ -100,14 +102,13 @@ const styles = StyleSheet.create({
   },
   animatedContainer: {
     backgroundColor: "#421b1b82",
-    height: 40,
-    width: 200,
-    position: "relative",
+    height: 50,
+    width: animatedContainerWidth,
+    left: width * 0.5 - animatedContainerWidth/2, 
+    position: "absolute",
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    // transform: [
-    //   { translateX: width * 0.5 - 20 },
-    // ],
+    zIndex: -1,
   },
 });
